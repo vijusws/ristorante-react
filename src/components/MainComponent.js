ba81c1920b7fcf863d3865f75cb41b7896afdd67
Vijetha <vijethak2@gmail.com>
2021-02-16 22:33:37 +0530

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import Home from "./HomeComponent";
 import Menu from "./MenuComponent";
+import Contact from "./ContactComponent";
 import DishDetail from "./DishdetailComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
@@ -13,27 +17,36 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
     };
     return (
       <div>
         <Header />
 
         <Switch>
-          {" "}
           //switch between the following 2 routes
           <Route path="/home" component={HomePage} />
           <Route
             exact
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
-          />{" "}
-          //to pass attribute i.e dishes, of Menu, too, this syntax is used
+          />
+          <Route exact path="/contactus" component={Contact} />} /> //to pass
+          //attribute i.e dishes, of Menu, too, this syntax is used
           <Redirect to="/home" />
         </Switch>
 
